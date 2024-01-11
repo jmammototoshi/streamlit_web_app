6b360320a8f52bf3aa97990ee15b0227ee9520af
jmammototoshi <mototoshi_isomura@jmam.co.jp>
2024-01-11 14:01:16 +0900

    page3 modify

--- pages/page_3.py
+++ pages/page_3.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 #データ分析関連
 #アプリディレクトリからのパスで指定する？
-df = pd.read_csv('./supu.app/data/気温.csv',index_col='月')
+df = pd.read_csv('./data/気温.csv',index_col='月')
 st.dataframe(df)
 st.bar_chart(df['2022'])
